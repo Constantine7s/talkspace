c4e1c3e5465101abf11b647cac2721e478def910
Konstantin Shambazov <k.shambazov@gmail.com>
2023-04-15 18:05:15 +0900

    feat: add cors and new routes routes

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+const cors = require('cors');
+const port = process.env.PORT || 8000;
+
+app.use(cors());
 
 const chats = require('./data');
-
-const port = process.env.PORT || 8000;
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
@@ -15,9 +17,9 @@ app.get('/api/chat', (req, res) => {
 });
 
 app.get('/api/chat/:id', (req, res) => {
-  const chat = chats.find((c) => c._id === req.params.id)
-  res.send(chat)
-})
+  const chat = chats.find((c) => c._id === req.params.id);
+  res.send(chat);
+});
 
 app.listen(port, () => {
   console.log('Your server is single and ready to mingle at port ' + port);
