a4a4e261d45d7468df7ea1d392f41eda51b8a584
Konstantin Shambazov <k.shambazov@gmail.com>
2023-04-20 12:21:47 +0900

    feat: add userController and userRoutes, update userModel

---
+++ backend/controllers/userController.js
@@ -0,0 +1,38 @@
+const asyncHandler = require('express-async-handler');
+const User = require('../models/userModel');
+
+const resgisterUser = asyncHandler(async (req, res) => {
+  const { name, email, password, pic } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error('Please enter all required information');
+  }
+
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error('User with this email already exists');
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    pic,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      email: user.email,
+      name: user.name,
+      pic: user.pic,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Failed to create user');
+  }
+});
+
+module.exports = {resgisterUser}
