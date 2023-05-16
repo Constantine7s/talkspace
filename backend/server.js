f6dc9cb4f333067e579175aa782b9b2bfb9f688a
Konstantin Shambazov <k.shambazov@gmail.com>
2023-05-16 22:24:35 +0900

    feat: add routes and controlles for messages api

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,30 @@
 const express = require('express');
 const app = express();
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
 const cors = require('cors');
 const port = process.env.PORT || 8000;
-const userRoutes = require('./routes/userRoutes')
-const chatRoutes = require('./routes/chatRoutes')
+const userRoutes = require('./routes/userRoutes');
+const chatRoutes = require('./routes/chatRoutes');
+const messageRoutes = require('./routes/messageRoutes');
 const connectDB = require('./config/db');
-const {errorHandler, notFound} = require('./middleware/errorMiddleware')
+const { errorHandler, notFound } = require('./middleware/errorMiddleware');
 
-dotenv.config()
-connectDB()
+dotenv.config();
+connectDB();
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
-app.use('/api/user', userRoutes)
-app.use('/api/chat', chatRoutes)
+app.use('/api/user', userRoutes);
+app.use('/api/chat', chatRoutes);
+app.use('/api/message', messageRoutes);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
-app.use(errorHandler)
-app.use(notFound)
+app.use(errorHandler);
+app.use(notFound);
 
 app.listen(port, () => {
   console.log('Your server is single and ready to mingle at port ' + port);
